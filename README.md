467404ceaaae06838399bbf9c33fec72dcadb9af
Guy Serfaty <guy@poob.me>
2020-04-27 19:37:55 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # nock-puppeteer
+Mock http requests with Nock.js in puppeteer
+
+## Motivation
+Integration/E2E tests and scrapers need to mock puppeteer's browser http requests - why not to have the abilities of Nock.js?
+"Nock works by overriding Node's http.request function" and when you run tests/scrapers with puppeteer the *browser* make the http requests 
+
+## How does it work?
+nock-puppeteer simply execute the requests instead of the browser with the help of puppeteer setRequestInterceptor hook.
+
+
+## Basic Usage
+const puppeteer = require('puppeteer');
  
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({path: 'example.png'});
+ 
+  await browser.close();
+})();
+
+
+## Basic Usage
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import * as nock from 'nock';
+import * as http from 'http';
+import * as https from 'https';
+
+const getRequestHandler = (allowedHosts: string[]) => (interceptedRequest: any) => {
+  const url: string = interceptedRequest.url()
+  const supportedResourceType = ['xhr', 'fetch'];
+  if (!supportedResourceType.includes(interceptedRequest.resourceType()) || !allowedHosts.find(allowedHost => url.includes(allowedHost))) {
+    return interceptedRequest.continue();
+  }
+  new Promise((resolve, reject) => {
+    let protocol: any = http;
+    if (url.includes('https://')) {
+      protocol = https;
+    }
+    const req: http.ClientRequest = protocol.request(url, {
+      method: interceptedRequest._method.toLowerCase(),
+      headers: interceptedRequest.headers(),
+    });
+    req.on('error', (e) => {
+      return reject(e)
+    });
+
+    req.on('response', response => {
+      let data = '';      
+      response.on('data', chunk => { data += chunk; });
+      response.on('end', () => {
+        const resolveValue = {
+          body: data,
+          status: response.statusCode,
+          headers: response.headers
+        };
+        resolve(resolveValue)
+      });
+    });
+    
+    const postData = interceptedRequest.postData();
+    if (postData) {
+      req.write(postData);
+    }
+    req.end();
+  }).then(value => {
+    return interceptedRequest.respond(value);
+  }).catch(e => {
+    // console.log('Error got', e);
+    interceptedRequest.abort();
+    throw e;
+  })
+};
+
+export default async (page: any, allowedHosts: string[]) => {
+  await page.setRequestInterception(true);
+  page.on('request', getRequestHandler(allowedHosts));
+  return nock
+}
