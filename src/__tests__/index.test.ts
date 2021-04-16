853f67476f79e641628a1e30df0d21630d06a9cc
Daniel Bidulock <daniel_bidulock@sil.org>
2021-04-16 15:47:56 -0600

    Add button clicks for triggering fetch requests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -16,13 +16,16 @@ test('Basic Mock', async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   useNock(page, ['https://example.com/api'])
-  
+
   const mockProducts = [{id: 1, product: 'A'}, {id: 2, product: 'B'}];
   await nock('https://example.com/api')
     .get('/api/products')
     .reply(200, mockProducts);
 
   await page.goto(mockPage);
+  await page.waitForSelector('#get-products');
+  await page.click('#get-products');
+
   const firstText = await getSelectorText(page, '.product-row')
   expect(firstText).toBe(mockProducts[0].product)
 
@@ -31,7 +34,36 @@ test('Basic Mock', async () => {
     .reply(200, [mockProducts[1]]);
 
   await page.goto(mockPage);
+  await page.waitForSelector('#get-products');
+  await page.click('#get-products');
+
   const secondText = await getSelectorText(page, '.product-row')
   expect(secondText).toBe(mockProducts[1].product)
+
   await browser.close();
+});
+
+it('Redirect Mock', async done => {
+  const mockPage = `file:${path.join(__dirname, 'test.html')}`
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  useNock(page, ['https://example.com', 'https://some-auth-provider.com/authorize'])
+
+  const redirectScope = nock('https://example.com')
+    .get('/login')
+    .reply(301, { 'Location': 'https://some-auth-provider.com' });
+
+//  const authScope = nock('https://some-auth-provider.com')
+//    .get('/authorize')
+//    .reply(200, {});
+
+  await page.goto(mockPage);
+
+  await page.click('#login');
+
+  expect(redirectScope.isDone()).toBe(true);;
+//  expect(authScope.isDone()).toBe(true);;
+  await browser.close();
+  done();
+
 });
