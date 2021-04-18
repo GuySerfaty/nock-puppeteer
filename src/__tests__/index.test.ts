1a36beabd90965111fef0bd592d17365b45e8a96
Guy Serfaty <guy@poob.me>
2021-04-18 18:51:25 +0300

    Make infra for adding more tests + create work flow on PR

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,77 +1,48 @@
-
 import * as puppeteer from 'puppeteer';
+import { Page, Browser } from 'puppeteer';
 import * as nock from 'nock';
-import * as path from 'path';
+import {
+  MOCK_PAGE_PATH, DEFAULT_PP_CONFIG
+} from './const';
 const useNock = require('../');
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+// const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 const getSelectorText = async (page: any, selector: string) => {
   await page.waitForSelector(selector);
   const element = await page.$(selector);
   return page.evaluate((pageElement: any) => pageElement.innerText, element);
 }
 
-test('Basic Mock', async () => {
-  console.log('Test 0 run')
-  const mockPage = `file:${path.join(__dirname, 'test.html')}`
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  useNock(page, ['https://example.com/api'])
+let browser: Browser;
+let page: Page;
+
+describe('Basic functionality', () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch(DEFAULT_PP_CONFIG);
+    page = await browser.newPage();
+  })
   
-  const mockProducts = [{id: 1, product: 'A'}, {id: 2, product: 'B'}];
-  await nock('https://example.com/api')
-    .get('/api/products')
-    .reply(200, mockProducts);
-
-  await page.goto(mockPage);
-  const firstText = await getSelectorText(page, '.product-row')
-  expect(firstText).toBe(mockProducts[0].product)
-
-  await nock('https://example.com/api')
-    .get('/api/products')
-    .reply(200, [mockProducts[1]]);
-
-  await page.goto(mockPage);
-  const secondText = await getSelectorText(page, '.product-row')
-  expect(secondText).toBe(mockProducts[1].product)
-  await browser.close();
-});
-
-test('Basic Mock2', async () => {
-  console.log('Test 2 run')
-  const mockPage = `file:${path.join(__dirname, 'test.html')}`
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  useNock(page, ['https://example.com/api'])
+  it('Basic Mock', async () => {
+    useNock(page, ['https://example.com/api'])
+    
+    const mockProducts = [{id: 1, product: 'A'}, {id: 2, product: 'B'}];
+    await nock('https://example.com/api')
+      .get('/api/products')
+      .reply(200, mockProducts);
   
-  const mockProducts: any = [{id: 1, product: 'C'}, {id: 2, product: 'D'}];;
-  await nock('https://example.com/api')
-    .get('/api/products')
-    .reply(200, mockProducts);
-
-  await sleep(5000);
-  await page.goto(mockPage);
-  const firstText = await getSelectorText(page, '.product-row')
-  expect(firstText).toBe(mockProducts[0].product)
-  await browser.close();
-});
-
-test('Basic Mock3', async () => {
-  console.log('Test 3 run')
-  const mockPage = `file:${path.join(__dirname, 'test.html')}`
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  useNock(page, ['https://example.com/api'])
+    await page.goto(MOCK_PAGE_PATH);
+    const firstText = await getSelectorText(page, '.product-row')
+    expect(firstText).toBe(mockProducts[0].product)
   
-  const mockProducts: any = [{id: 1, product: 'E'}, {id: 2, product: 'F'}];;
-  await nock('https://example.com/api')
-    .get('/api/products')
-    .reply(200, mockProducts);
+    await nock('https://example.com/api')
+      .get('/api/products')
+      .reply(200, [mockProducts[1]]);
+  
+    await page.goto(MOCK_PAGE_PATH);
+    const secondText = await getSelectorText(page, '.product-row')
+    expect(secondText).toBe(mockProducts[1].product)
+    await browser.close();
+  });
+})
 
-  await sleep(5000);
-  await page.goto(mockPage);
-  const firstText = await getSelectorText(page, '.product-row')
-  expect(firstText).toBe(mockProducts[0].product)
-  await browser.close();
-});
 
