edc18a768050bd47bca0c8bed5578ddc872a6d05
Guy Serfaty <guy@poob.me>
2021-04-19 18:17:34 +0300

    bump package version + formatting

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import * as http from 'http';
 import * as https from 'https';
 
 const getRequestHandler = (allowedHosts: string[]) => (interceptedRequest: any) => {
-  const url: string = interceptedRequest.url()
+  const url: string = interceptedRequest.url();
   const supportedResourceType = ['xhr', 'fetch', 'document'];
-  if (!supportedResourceType.includes(interceptedRequest.resourceType()) || !allowedHosts.find(allowedHost => url.includes(allowedHost))) {
+  if (
+    !supportedResourceType.includes(interceptedRequest.resourceType()) ||
+    !allowedHosts.find((allowedHost) => url.includes(allowedHost))
+  ) {
     return interceptedRequest.continue();
   }
   new Promise((resolve, reject) => {
@@ -17,37 +20,41 @@ const getRequestHandler = (allowedHosts: string[]) => (interceptedRequest: any) 
       headers: interceptedRequest.headers(),
     });
     req.on('error', (e) => {
-      return reject(e)
+      return reject(e);
     });
 
-    req.on('response', response => {
-      let data = '';      
-      response.on('data', chunk => { data += chunk; });
+    req.on('response', (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
       response.on('end', () => {
         const resolveValue = {
           body: data,
           status: response.statusCode,
-          headers: response.headers
+          headers: response.headers,
         };
-        resolve(resolveValue)
+        resolve(resolveValue);
       });
     });
-    
+
     const postData = interceptedRequest.postData();
     if (postData) {
       req.write(postData);
     }
     req.end();
-  }).then(value => {
-    return interceptedRequest.respond(value);
-  }).catch(e => {
-    // console.log('Error got', e);
-    interceptedRequest.abort();
-    throw e;
   })
+    .then((value) => {
+      return interceptedRequest.respond(value);
+    })
+    .catch((e) => {
+      // console.log('Error got', e);
+      interceptedRequest.abort();
+      throw e;
+    });
 };
 
 export default async (page: any, allowedHosts: string[]) => {
   await page.setRequestInterception(true);
   page.on('request', getRequestHandler(allowedHosts));
-}
+};
